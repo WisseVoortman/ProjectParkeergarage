a938ffe9ae127115411a70774b4751f1e02addfc
Mitchel van Rijn <mitchelvanrijn@hotmail.nl>
2017-01-19 14:24:54 +0100

    Reverted previous commit. Whoops!

--- TestorMain.java
+++ TestorMain.java
@@ -2,10 +2,8 @@
  * Created by root on 19-Jan-17.
  */
 public class TestorMain {
-    public static int main(String[] args) {
+    public static void main(String[] args) {
         //- Start the program!
         Testor testor = new Testor();
-
-        return 0;
     }
 }
