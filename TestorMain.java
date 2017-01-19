567f0617d2c6440904880836b48d2d3e0ba5a6ed
Mitchel van Rijn <mitchelvanrijn@hotmail.nl>
2017-01-19 14:09:59 +0100

    Changed main to implement an int return.

--- TestorMain.java
+++ TestorMain.java
@@ -2,8 +2,10 @@
  * Created by root on 19-Jan-17.
  */
 public class TestorMain {
-    public static void main(String[] args) {
+    public static int main(String[] args) {
         //- Start the program!
         Testor testor = new Testor();
+
+        return 0;
     }
 }
