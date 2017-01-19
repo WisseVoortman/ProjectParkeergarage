bcb7b435110da3e5a9ebd88408cc1a6e36d3c4c0
Mitchel van Rijn <mitchelvanrijn@hotmail.nl>
2017-01-19 13:21:04 +0100

    Added a testor class.

---
+++ Testor.java
@@ -0,0 +1,8 @@
+/**
+ * Created by root on 19-Jan-17.
+ */
+public class Testor {
+    public static void main(String[] args) {
+        System.out.println( "Hier, kijk deze shit! Bam!" );
+    }
+}
