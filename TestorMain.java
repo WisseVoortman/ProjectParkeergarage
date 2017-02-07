0d3bf20bbcbd397b7f4c1e36a9f2204167eab68c
Mitchel van Rijn <mitchelvanrijn@hotmail.nl>
2017-02-07 09:48:14 +0100

    Removed Testor.

--- TestorMain.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by root on 19-Jan-17.
- */
-public class TestorMain {
-    public static void main(String[] args) {
-        //- Start the program!
-        Testor testor = new Testor();
-    }
-}
